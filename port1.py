a03d89cb0f7431a126358e2364b8baad7ab2c3b9
dastangoo <hamed.dastangoo>
2017-04-07 11:10:46 +0430

    Some changes to port1.py

--- port1.py
+++ port1.py
@@ -10,12 +10,18 @@ def portfolio_cost(filename):
     total = 0.0
     with open(filename, 'r') as f:
         rows = csv.reader(f)
-        headers = next(rows)                      # Skip the header row
-        for row in rows:
-            row[2] = int(row[2])
-            row[3] = float(row[3])
-            total += row[2] * row[3]
+        headers = next(rows)
+        for rowno, row in enumerate(rows, start = 1):
+            try:
+                row[2] = int(row[2])
+                row[3] = float(row[3])
+                total += row[2] * row[3]
+            except ValueError as err:
+                print('Row:', rowno, 'Bad row:', row)
+                print('Row:', rowno, 'Reason:', err)
+                #continue
+                #total += row[2] * row[3]
     return total
 
-total = portfolio_cost('Data/portfolio.csv')
+total = portfolio_cost('Data/missing.csv')
 print('Total cost: ', total)
