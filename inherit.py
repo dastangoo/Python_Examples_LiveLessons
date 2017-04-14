d2bfa67ab60633eaec4012825d5aa8220a5683bc
dastangoo <hamed.dastangoo@gmail.com>
2017-04-14 20:48:54 +0430

    Commit for inherit.py which introduces different types of inheritance

---
+++ inherit.py
@@ -0,0 +1,11 @@
+class Parent(object):
+    def __init__(self, value):
+        self.value = value
+
+    def spam(self):
+        print('Parnet.spam', self.value)
+
+    def grok(self):
+        print('Parent.grok')
+        self.spam()
+        
