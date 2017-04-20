ee950b1acb73225537dfbfbd3165f7fee429e469
dastangoo <hamed.dastangoo@gmail.com>
2017-04-20 19:12:55 +0430

    Some notes about how overloading typical behavior of Python magic funcitons in a custom container

--- holding.py
+++ holding.py
@@ -27,4 +27,31 @@ def read_portfolio(filename):
             portfolio.append(h)
     return portfolio
 
-portfolio = read_portfolio('Data/portfolio.csv')
+
+class Portfolio(object):
+    def __init__(self):
+        self.holdings = []
+    @classmethod
+    def from_csv(cls, filename):
+        self = cls()
+        with open(filename, 'r') as f:
+            rows = csv.reader(f)
+            headers = next(rows)
+            for row in rows:
+                h = Holding(row[0], row[1], int(row[2]), float(row[3]))
+                self.holdings.append(h)
+        return self
+
+    def total_cost(self):
+        return sum([h.shares * h.price for h in self.holdings])
+    def current_value(slef):
+        pass
+    def __len__(self):
+        return len(self.holdings)
+    def __getitem__(self, n):
+        if isinstance(n, str):
+            return [ h for h in self.holdings if h.name == n]
+        else:
+            return self.holdings[n]
+    def __iter__(self):
+        return self.holdings.__iter__()
