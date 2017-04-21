ee7e6367773aff14bcf444b6a8af9a240546e2ee
dastangoo <hamed.dastangoo@gmail.com>
2017-04-21 13:16:34 +0430

    Some notes about meta class

--- table.py
+++ table.py
@@ -1,46 +1,34 @@
 # table.py
 import sys
-from abc import ABC, abstractmethod
 def print_table(objects, colnames, formatter):
     '''
     Make a nicely formatted table showing attributes from a list of objects
     '''
-
-    if not isinstance(formatter, TableFormatter):
-        raise TypeError('formatter must be a TableFormatter')
-
     formatter.headings(colnames)
 
     for obj in objects:
         rowdata = [str(getattr(obj, colname)) for colname in colnames]
         formatter.row(rowdata)
+_formatters = {}
+class TableMeta(type):
+    def  __init__(cls, clsname, bases, methods):
+        super().__init__(clsname, bases, methods)
+        if hasattr(cls, 'name'):
+            _formatters[cls.name] = cls
 
-
-class TableFormatter(ABC):
+class TableFormatter(metaclass=TableMeta):
     def __init__(self, outfile=None):
         if outfile == None:
             outfile = sys.stdout
         self.outfile = outfile
     # Serves a design spec for making tables (use inheritance to customize)
-    @abstractmethod
     def headings(self, headers):
-        pass
-    @abstractmethod
+        raise NotImplementedError
     def row(self, rowdata):
-        pass
-    def print_table(self, objects, colnames):
-        '''
-        Make a nicely formatted table showing attributes from a list of objects
-        '''
-        self.headings(colnames)
-
-        for obj in objects:
-            rowdata = [str(getattr(obj, colname)) for colname in colnames]
-            self.row(rowdata)
-
-
+        raise NotImplementedError
 
 class TextFormatter(TableFormatter):
+    name = 'text'
     def __init__(self, outfile=None, width=10):
         super().__init__(outfile)   # Initialize parent
         self.width = width
@@ -48,18 +36,20 @@ class TextFormatter(TableFormatter):
         for header in headers:
             print('{:>{}s}'.format(header, self.width), end=' ', file=self.outfile)
         print(file=self.outfile)
+
     def row(self, rowdata):
         for item in rowdata:
             print('{:>{}s}'.format(item, self.width), end=' ', file=self.outfile)
         print(file=self.outfile)
         ...
-class CSVFormatter(object):
+class CSVFormatter(TableFormatter):
+    name = 'csv'
     def headings(self, headers):
         print(','.join(headers))
     def row(self, rowdata):
         print(','.join(rowdata))
-
 class HTMLFormatter(TableFormatter):
+    name = 'html'
     def headings(self, headers):
         print('<tr>', end='')
         for h in headers:
@@ -75,15 +65,8 @@ class QuotedMixin(object):
         quoted = [ '"{}"'.format(d) for d in rowdata]
         super().row(quoted)
 
-class TablePrinter(object):
-    def __init__(self, formatter):
-        self.formatter = formatter
-    def print_table(self, objects, colnames):
-        '''
-        Make a nicely formatted table showing attributes from a list of objects
-        '''
-        self.formatter.headings(colnames)
-
-        for obj in objects:
-            rowdata = [str(getattr(obj, colname)) for colname in colnames]
-            self.formatter.row(rowdata)
+def create_formatter(name):
+    formatter = _formatters.get(name)
+    if not formatter:
+        raise ValueError('Unknown format {}'.format(name))
+    return formatter()
