1ef17e4670bb11a0789e3f1aa0ef955aed42cbe6
dastangoo <hamed.dastangoo@gmail.com>
2017-04-21 09:46:39 +0430

    Some notes about decorators and wrappers

---
+++ sample.py
@@ -0,0 +1,13 @@
+from logcall import logged
+@logged
+def add(x, y):
+    '''
+    Adds x adn y
+    '''
+    return x + y
+@logged
+def sub(x,y):
+    return x - y
+@logged
+def mul(x, y):
+    return x * y
