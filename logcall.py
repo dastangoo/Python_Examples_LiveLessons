76264576f5198ad1db7f62ecfcc8087ce5803beb
dastangoo <hamed.dastangoo@gmail.com>
2017-04-21 10:29:17 +0430

    More discussions about decorators and how use arguments with them

--- logcall.py
+++ logcall.py
@@ -1,12 +1,14 @@
 # logcall.py
 from functools import wraps
 
-def logged(func):
-    # Idea: Give me a function, I'll put logging around it
+def logformat(fmt):
+    def logged(func):
+        # Idea: Give me a function, I'll put logging around it
 
-    print('Adding logging to', func.__name__)
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        print('You called', func.__name__)
-        return func(*args, **kwargs)
-    return wrapper
+        print('Adding logging to', func.__name__)
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            print(fmt.format(func=func))
+            return func(*args, **kwargs)
+        return wrapper
+    return logged
--- sample.py
+++ sample.py
@@ -1,13 +1,18 @@
-from logcall import logged
+from logcall import logformat
+
+logged = logformat('YOU ARE CALLING {func.__name__}')
+
 @logged
 def add(x, y):
     '''
     Adds x adn y
     '''
     return x + y
+
 @logged
 def sub(x,y):
     return x - y
+
 @logged
 def mul(x, y):
     return x * y
