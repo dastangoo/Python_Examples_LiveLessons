e2bf7c5d28b54b518ef197aa0bc20aa4b7430944
dastangoo <hamed.dastangoo@gmail.com>
2017-04-21 14:56:57 +0430

    More tweaks about meta class and its use with class decorators

--- validate.py
+++ validate.py
@@ -1,7 +1,7 @@
 # validate.py
 class Typed(object):
     expected_type = object
-    def __init__(self, name):
+    def __init__(self, name=None):
         self.name = name
     def __get__(self, instance, cls):
         return instance.__dict__[self.name]
@@ -18,27 +18,37 @@ class String(Typed):
     expected_type = str
 
 def typed(cls):
+    cls._attributes = set()
     for key, value in vars(cls).items():
         if isinstance(value, Typed):
             value.name = key
+            cls._attributes.add(key)
     return cls
+class structuretype(type):
+    def __new__(meta, name, bases, methods):
+        cls = super().__new__(meta, name, bases, methods)
+        cls = typed(cls)    # Apply a class decorator
+        return cls
+
+class Structure(metaclass=structuretype):
+    def __setattr__(self, name, value):
+        if name not in self._attributes:
+            raise AttributeError('No attribute {}'.format(name))
+        super().__setattr__(name, value)
+
 
 @typed
-class Holding(object):
+class Holding(Structure):
     name = String()
     date = String()
     shares = Integer()
     price = Float()
-
     def __init__(self, name, date, shares, price):
         self.name = name
         self.date = date
         self.shares = shares
         self.price = price
-    def __setattr__(self, name, value):
-        if name not in {'name', 'date', 'shares', 'price'}:
-            raise AttributeError('No attribute {}'.format(name))
-        super().__setattr__(name, value)
+
     @property
     def cost(self):
         return self.shares * self.price
